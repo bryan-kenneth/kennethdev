7e31a3b2db488b388923313ca8d1b086250cfe16
Bryan Munoz <bry4n.munoz@gmail.com>
2021-03-19 18:41:32 -0700

    added resume

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
+import resume from './MunozResume.pdf';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                Hello World
+                <button onClick={() => window.open(resume, '_blank')}>
+                    Resume
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
